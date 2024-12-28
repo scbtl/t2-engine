568855411f0a9195951df351200950755f810c9b
Tony <57773117@qq.com>
2024-12-28 12:33:42 +0800

    database

--- config/database.php
+++ config/database.php
@@ -12,4 +12,55 @@
  * @license   http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
-return [];
+return [
+    // 默认数据库
+    'default'     => 'mysql',
+    // 各种数据库配置
+    'connections' => [
+
+        'mysql' => [
+            'driver'      => 'mysql',
+            'host'        => '127.0.0.1',
+            'port'        => 3306,
+            'database'    => 'webman',
+            'username'    => 'webman',
+            'password'    => '',
+            'unix_socket' => '',
+            'charset'     => 'utf8',
+            'collation'   => 'utf8_unicode_ci',
+            'prefix'      => '',
+            'strict'      => true,
+            'engine'      => null,
+        ],
+
+        'sqlite' => [
+            'driver'   => 'sqlite',
+            'database' => '',
+            'prefix'   => '',
+        ],
+
+        'pgsql' => [
+            'driver'   => 'pgsql',
+            'host'     => '127.0.0.1',
+            'port'     => 5432,
+            'database' => 'webman',
+            'username' => 'webman',
+            'password' => '',
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'public',
+            'sslmode'  => 'prefer',
+        ],
+
+        'sqlsrv' => [
+            'driver'   => 'sqlsrv',
+            'host'     => 'localhost',
+            'port'     => 1433,
+            'database' => 'webman',
+            'username' => 'webman',
+            'password' => '',
+            'charset'  => 'utf8',
+            'prefix'   => '',
+        ],
+    ],
+];
