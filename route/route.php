2ea6b595ec8d5c5125316253dd20910211f1a497
Tony <57773117@qq.com>
2025-01-17 07:33:49 +0800

    Initial commit

---
+++ route/route.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of t2.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the MIT-LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author    Tony<lucky@t2engine.cn>
+ * @copyright Tony<lucky@t2engine.cn>
+ * @link      http://www.t2engine.cn/
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+use t2\Route;
+
+// 禁用主项目默认路由，不影响应用插件
+//Route::disableDefaultRoute();
