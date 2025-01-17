d7d2ad3f62a7f6d7b0f481c786e63359be19004e
Tony <57773117@qq.com>
2025-01-17 14:11:50 +0800

    functions.php

--- app/functions.php
+++ app/functions.php
@@ -2,28 +2,3 @@
 /**
  * Here is your custom functions.
  */
-
-if (!function_exists('dumpx')) {
-    /**
-     * 封装 dump 函数，追加调用行号信息
-     *
-     * @param mixed ...$vars
-     * @return mixed
-     */
-    function dumpx(mixed ...$vars): mixed
-    {
-        // 获取调用栈
-        $backtrace = debug_backtrace();
-        $caller = $backtrace[0]; // dumpx 的调用者信息
-        $line = $caller['line']; // 调用行号
-        $file = $caller['file']; // 获取文件名
-
-        // 调用原 dump 函数
-        $result = dump(...$vars);
-
-        // 在最后输出行号信息
-        echo "Called at $file on line $line:\n";
-
-        return $result;
-    }
-}
